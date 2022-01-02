e9ded596826e86b2ee7fa017db157d070aade642
Sahil <Sahil@dotcode.com>
2022-01-02 17:11:34 +0600

    exercise or solution 04 class number 33 or 34

--- exercise04.py
+++ exercise04.py
@@ -3,14 +3,67 @@
 # thislist = ['name','email','password','phone']
 # thistuple = ('dot_code','dotcode@gmail.com','xxxxxxx','xxxxxxxxxxx')
 
-# for
+# for / while
 
 # thisdict ={'name': 'dot_code', 'email': 'dotcode@gmail.com', 'password': 'xxxxxxx', 'phone': 'xxxxxxxxxxx'}
 
 # task 02
 # thisdict ={'name': 'dot_code', 'email': 'dotcode@gmail.com', 'password': 'xxxxxxx', 'phone': 'xxxxxxxxxxx'}
+
 # thislist = ['name','email','password','phone']
 # thistuple = ('dot_code','dotcode@gmail.com','xxxxxxx','xxxxxxxxxxx')
+
+
+
+
+
+
+# task 01
+# thislist = ['name','email','password','phone']
+# thistuple = ('dot_code','dotcode@gmail.com','xxxxxxx','xxxxxxxxxxx')
+
+# thisdict = {}
+
+# if len(thislist) == len(thistuple):
+#     for i in range(len(thislist)):
+#         thisdict.update({thislist[i]:thistuple[i]})
+#     print(thisdict)
+# else:
+#     print('The lenth of list is not equall to tuple .')
+
+
+
+# for / while
+
+# thisdict ={'name': 'dot_code', 'email': 'dotcode@gmail.com', 'password': 'xxxxxxx', 'phone': 'xxxxxxxxxxx'}
+
+
+
+
+
+# task 02
+thisdict ={'name': 'dot_code', 'email': 'dotcode@gmail.com', 'password': 'xxxxxxx', 'phone': 'xxxxxxxxxxx'}
+
+thislist = []
+thistuple = ()
+ct = list(thistuple)
+
+for x,y in thisdict.items():
+    thislist.append(x)
+    ct.append(y)
+
+thistuple = tuple(ct)
+
+print(thislist)
+print(thistuple)
+
+
+# thislist = ['name','email','password','phone']
+# thistuple = ('dot_code','dotcode@gmail.com','xxxxxxx','xxxxxxxxxxx')
+
+
+
+
 
 
 
