6068ea7e3a0a73cf3ac5012449b99d3ce31a55fa
Sahil <Sahil@dotcode.com>
2022-02-14 17:12:48 +0600

    kjkjl

---
+++ folder03/class_mathod.py
@@ -0,0 +1,59 @@
+class Car:
+    offer = 12
+    def __init__(self,name,brand,price):
+        self.name = name
+        self.brand = brand
+        self.price = price
+    
+    @classmethod
+    def disc_price():
+        # d = self.price - (self.price*Car.offer/100)
+        d = Car.price - (Car.price*Car.offer/100)
+        return d
+
+c = Car('Tesla Model X','Tesla',75000)
+# Car.offer += 100
+c.offer += 100
+c1 = Car('Tesla Model S','Tesla',65000)
+
+
+print(Car.disc_price(c))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Phone:
+#     offer = 10
+#     def __init__(self,name,brand,price):
+#         self.name = name 
+#         self.brand = brand
+#         self.price = price
+
+#     def disc_price(self):
+#         p = self.price - int(self.price*self.offer/100)
+#         return p
+
+# phone1 = Phone('iPhone','Apple',120000)
+# phone1.offer = 20
+# print(phone1.disc_price())
+
