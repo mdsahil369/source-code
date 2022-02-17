8c25fbc0943182f7b8bd8e36b2dd24b021419827
Sahil <Sahil@dotcode.com>
2022-02-17 20:13:12 +0600

    kjkjl

--- folder04/inharitance.py
+++ folder04/inharitance.py
@@ -1,29 +1,27 @@
+class String:
+    def __init__(self,string,string1):
+        self.string = string
+        self.string1 = string1
+    
+    def lenth(self):
+        lenth = 0
+        for i in self.string:
+            lenth += 1
+        return lenth 
 
+class String1(String):
+    def __init__(self, string,string1, number):
+        super().__init__(string,string1)
+        # String.__init__(self,string,string1)
+        self.number = number
 
-# -------------------------- Laptop -------------------
+# s = String("Wikipedia is a free online encyclopedia.")
+s = String1("Wikipedia is a free online encyclopedia.","jlfjljfl",7)
 
-class Laptop:
-    def __init__(self,name,brand,price):
-        self.name = name
-        self.brand = brand
-        self.price = price
-        self.alldata = f"This Laptop Name : {self.name} ,Brand : {self.brand} And Price : {self.price}"
+print(s.string1)
 
-laptop = Laptop('Dell XPS','Dell','1,27,000.00')
-
-
-class Letest_Laptop(Laptop):
-    def __init__(self,name,brand,price,ram,storage):
-        Laptop.__init__(self,name,brand,price)
-        self.ram = ram
-        self.storage = storage
-
-laptop0 = Laptop('Dell XPS','Dell','1,27,000.00')
-laptop1 = Letest_Laptop('Dell XPS 2','Dell2','21,27,000.00','64 GB','1 TB')
-
-print(laptop1.alldata)
-
-
+# print(s.string)
+# print(s.lenth())
 
 
 
---
+++ folder04/omr,methodoverr.py
@@ -0,0 +1,39 @@
+class String:
+    def __init__(self,string):
+        self.string = string
+    
+    def lenth(self):
+        lenth = 0
+        for i in self.string:
+            lenth += 1
+        return lenth 
+
+class String1(String):
+    def __init__(self, string, number):
+        super().__init__(string)
+        self.number = number
+    def word(self):
+        word = 0
+        st = self.string.split(" ")
+        for i in st:
+            word += 1
+        return word
+
+class String2(String1):
+    def __init__(self, string, number, boolean):
+        super().__init__(string, number)
+        self.boolean = boolean
+    
+    def upper(self):
+        return self.string.upper()
+
+s = String1("Wikipedia is a free online encyclopedia.",7)
+
+# print(s.string)
+# print(s.lenth())
+# print(s.word())
+# print(s.upper())
+
+# print(help(String1))
+# print(isinstance(s,String2))
+print(issubclass(String1,String2))
